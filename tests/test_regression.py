09be7fafda3552d265ce8e96f3415664fbcd2375
Justin Weinmeister <weinmeistejr@ornl.gov>
2026-02-10 22:24:30 -0500

    add liteature regression test

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -1,6 +1,8 @@
 from pytest import approx
+
 import cfdverify.utils as utils
-from cfdverify.discretization import Classic
+import cfdverify.discretization as dis
+
 
 def test_asme_procedure_phi1():
     """Test against ASME JFE annoucement
@@ -21,12 +23,13 @@ def test_asme_procedure_phi1():
     V = 1 # placeholder volume
     mesh_sizes = utils.mesh_size(V, N, 2)
     phi1 = [6.063, 5.972, 5.863]
-    model = Classic(mesh_sizes, phi1)
+    model = dis.Classic(mesh_sizes, phi1)
     assert model.refinement_ratios == approx([1.5, 1.333], abs=0.0005)
     assert model.order["System Response Quantity"] == approx(1.53, abs=0.005)
     assert model.f_est["System Response Quantity"] == approx(6.1685,
                                                              abs=0.00005)
     assert model.uncertainty("System Response Quantity", 0, normalize=True) == approx(0.022, abs=0.0005)
+
 
 def test_asme_procedure_phi2():
     """Test against ASME JFE annoucement
@@ -47,9 +50,17 @@ def test_asme_procedure_phi2():
     V = 1 # placeholder volume
     mesh_sizes = utils.mesh_size(V, N, 2)
     phi1 = [10.7880, 10.7250, 10.6050]
-    model = Classic(mesh_sizes, phi1)
+    model = dis.Classic(mesh_sizes, phi1)
     assert model.refinement_ratios == approx([2.0, 2.143], abs=0.0005)
     assert model.order["System Response Quantity"] == approx(0.75, abs=0.005)
     assert model.f_est["System Response Quantity"] == approx(10.8801,
                                                              abs=0.00005)
     assert model.uncertainty("System Response Quantity", 0, normalize=True) == approx(0.011, abs=0.0005)
+
+
+def test_first_and_second_order_literature(roy_2003):
+    roy_data = roy_2003[0]
+    roy_exact = roy_2003[1]
+    model = dis.CustomDiscretizationError(roy_data, model=dis.FirstAndSecondOrder)
+
+    assert model.f_est.values == approx(roy_exact, rel=0.05)
